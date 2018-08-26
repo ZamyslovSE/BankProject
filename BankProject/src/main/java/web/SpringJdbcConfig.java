f2580ec34e40e5464c2243ab92adab2e05f85ddf
ZamyslovSE <zamyslov.stepan@gmail.com>
2018-08-26 22:38:09 +0300

    remote db

--- BankProject/src/main/java/web/SpringJdbcConfig.java
+++ BankProject/src/main/java/web/SpringJdbcConfig.java
@@ -26,8 +26,8 @@ public class SpringJdbcConfig {
 //        return dataSource;
         return DataSourceBuilder.create()
                 .driverClassName("com.mysql.jdbc.Driver")
-                .url("jdbc:mysql://localhost:3306/BankDBTest?serverTimezone=UTC")
-                .username("root")
-                .password("root").build();
+                .url("jdbc:mysql://ec2-18-220-114-39.us-east-2.compute.amazonaws.com:3306/BankDBTest?serverTimezone=UTC")
+                .username("bank_server")
+                .password("interiorlikesuicidewristred").build();
     }
 }
